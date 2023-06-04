eb25ea24ab07f963e40de0f725c85dbe8e2d6c98
klemonade <lemon.thanakrit@gmail.com>
2023-06-04 21:44:17 +0700

    feat: add pwa from cgmparent

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,39 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		createHtmlPlugin({
+			minify: true,
+		}),
+		SvelteKitPWA(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Idol Board',
+				short_name: 'IdolBoard',
+				description: 'Name Board for Idol',
+				categories: ['idol', 'fanclub', 'cgm48'],
+				theme_color: '#1d618e',
+				background_color: '#2e2e34',
+				display: 'standalone',
+				orientation: 'landscape',
+				id: '/',
+				scope: '/',
+				start_url: '/',
+			},
+		})
 	],
+	server: {
+		port: 4127,
+	},
+	build: {
+		target: 'esnext',
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
