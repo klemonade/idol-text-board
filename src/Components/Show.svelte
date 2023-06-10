038177900298b39ba08a094eeca4d66cbc15b1d0
klemonade <lemon.thanakrit@gmail.com>
2023-06-10 09:37:16 +0700

    feat: add static page

--- src/Components/Show.svelte
+++ src/Components/Show.svelte
@@ -1,53 +1,66 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import Text1 from "./ShowText/Text1.svelte";
-	import Text2 from "./ShowText/Text2.svelte";
-	import Text3 from "./ShowText/Text3.svelte";
-	import Text4 from "./ShowText/Text4.svelte";
-	import Text5 from "./ShowText/Text5.svelte";
-	import Text6 from "./ShowText/Text6.svelte";
-	import Text7 from "./ShowText/Text7.svelte";
-	import Text8 from "./ShowText/Text8.svelte";
-	import Text9 from "./ShowText/Text9.svelte";
-	import Text10 from "./ShowText/Text10.svelte";
-	import { Button } from "flowbite-svelte";
-    export let name: string | null;
-    export let cnt: number
-    const handleNext = () => {
-        cnt += 1
-        cnt = cnt % 20
-    }
+	import { onMount } from 'svelte';
+	import Text0 from './ShowText/Text0.svelte';
+	import Text1 from './ShowText/Text1.svelte';
+	import Text2 from './ShowText/Text2.svelte';
+	import Text3 from './ShowText/Text3.svelte';
+	import Text4 from './ShowText/Text4.svelte';
+	import Text5 from './ShowText/Text5.svelte';
+	import Text6 from './ShowText/Text6.svelte';
+	import Text7 from './ShowText/Text7.svelte';
+	import Text8 from './ShowText/Text8.svelte';
+	import Text9 from './ShowText/Text9.svelte';
+	import Text10 from './ShowText/Text10.svelte';
+	import { Button } from 'flowbite-svelte';
+	export let name: string | null;
+	export let cnt: number;
+	const handleNext = () => {
+		cnt += 1;
+		cnt = cnt % 20;
+	};
 </script>
+
 <section class="text-[10vw] w-[100%] h-screen overflow-hidden">
-<!-- <div class="translate-x-[-50%] left-[50%] translate-y-[-50%] top[50%] h-fit"> -->
-    {#if cnt == 0}
-    <Text1 {name} on:next={handleNext}/>
-    {:else if cnt == 1}
-    <Text2 {name} on:next={handleNext}/>
-    {:else if cnt == 2}
-    <Text3 {name} on:next={handleNext} />
-    {:else if cnt == 3}
-    <Text4 {name} on:next={handleNext}/>
-    {:else if cnt == 4}
-    <Text5 {name} on:next={handleNext}/>
-    {:else if cnt == 5}
-    <Text6 {name} on:next={handleNext}/>
-    {:else if cnt == 6}
-    <Text7 {name} on:next={handleNext}/>
-    {:else if cnt == 7}
-    <Text8 {name} on:next={handleNext}/>
-    {:else if cnt == 8}
-    <Text9 {name} on:next={handleNext}/>
-    {:else if cnt == 9}
-    <Text10 {name} on:next={handleNext}/>
-    <!-- {:else if cnt == 1}
+	<!-- <div class="translate-x-[-50%] left-[50%] translate-y-[-50%] top[50%] h-fit"> -->
+	{#if cnt == 0}
+		<Text0 {name} on:next={handleNext} />
+	{:else if cnt == 1}
+		<Text1 {name} on:next={handleNext} />
+	{:else if cnt == 2}
+		<Text2 {name} on:next={handleNext} />
+	{:else if cnt == 3}
+		<Text3 {name} on:next={handleNext} />
+	{:else if cnt == 4}
+		<Text4 {name} on:next={handleNext} />
+	{:else if cnt == 5}
+		<Text5 {name} on:next={handleNext} />
+	{:else if cnt == 6}
+		<Text6 {name} on:next={handleNext} />
+	{:else if cnt == 7}
+		<Text7 {name} on:next={handleNext} />
+	{:else if cnt == 8}
+		<Text8 {name} on:next={handleNext} />
+	{:else if cnt == 9}
+		<Text9 {name} on:next={handleNext} />
+	{:else if cnt == 10}
+		<Text10 {name} on:next={handleNext} />
+		<!-- {:else if cnt == 1}
     <Text2 {name} on:next={handleNext}/>
     <p>3</p> -->
-    {/if}
-<!-- </div> -->
-<div class="absolute bottom-0 w-screen justify-between flex flex-row p-8 text-base sm:text-xl">
-    <Button color="alternative" on:click={() => {cnt = (cnt - 1) < 0 ? 9: cnt - 1}}>{"<"}</Button>
-    <Button color="alternative" on:click={() => {cnt = (cnt + 1) > 9 ? 0: cnt + 1}}>{">"}</Button>
-</div>
+	{/if}
+	<!-- </div> -->
+	<div class="absolute bottom-0 w-screen justify-between flex flex-row p-8 text-base sm:text-xl">
+		<Button
+			color="alternative"
+			on:click={() => {
+				cnt = cnt - 1 < 0 ? 10 : cnt - 1;
+			}}>{'<'}</Button
+		>
+		<Button
+			color="alternative"
+			on:click={() => {
+				cnt = cnt + 1 > 10 ? 0 : cnt + 1;
+			}}>{'>'}</Button
+		>
+	</div>
 </section>
-
---
+++ src/Components/ShowText/Text0.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher()
+    export let name
+</script>
+
+<div class="w-[100%] h-[100%] flex flex-col justify-center items-center">
+    <h1 class="ml1">
+      <span class="text-wrapper">
+        <span class="line line1"></span>
+        <span class="letters">{name}</span>
+        <span class="line line2"></span>
+      </span>
+    </h1>
+</div>
+
+<style>
+    .ml1 {
+  font-weight: 900;
+  font-size: 1.9em;
+    }
+
+    .ml1 .letter {
+    display: inline-block;
+    line-height: 1em;
+    }
+
+    .ml1 .text-wrapper {
+    position: relative;
+    display: inline-block;
+    padding-top: 0.1em;
+    padding-right: 0.05em;
+    padding-bottom: 0.15em;
+    }
+
+    .ml1 .line {
+    opacity: 100;
+    position: absolute;
+    left: 0;
+    height: 8px;
+    width: 100%;
+    background-color: #fff;
+    transform-origin: 0 0;
+    }
+
+    .ml1 .line1 { top: 0; }
+    .ml1 .line2 { bottom: 0; }
+</style>
